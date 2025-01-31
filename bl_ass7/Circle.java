dc195d8a752ecff59dcbaa113ee6be843e2c75f8
samridhi singh <gurjarsam6@gmail.com>
2025-01-31 14:34:58 +0530

    circle details'

---
+++ bl_ass7/Circle.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+class Circle {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double calculateArea() {
+        return Math.PI * radius * radius;
+    }
+
+    public double calculateCircumference() {
+        return 2 * Math.PI * radius;
+    }
+
+    public void displayDetails() {
+        System.out.println("Circle Details:");
+        System.out.println("Radius: " + radius);
+        System.out.println("Area: " + calculateArea());
+        System.out.println("Circumference: " + calculateCircumference());
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the radius of the circle: ");
+        double radius = scanner.nextDouble();
+
+        Circle circle = new Circle(radius);
+        circle.displayDetails();
+
+        scanner.close();
+    }
+}
