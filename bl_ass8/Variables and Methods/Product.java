e5f93bfa68c2aeb5488960cc6921fb8711c6f3bc
samridhi singh <gurjarsam6@gmail.com>
2025-02-02 21:50:03 +0530

    product

---
+++ bl_ass8/Variables and Methods/Product.java
@@ -0,0 +1,48 @@
+public class Product {
+    private String productName;
+    private double price;
+    private static int totalProducts = 0;
+
+    public Product(String productName, double price) {
+        this.productName = productName;
+        this.price = price;
+        totalProducts++;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void displayProductDetails() {
+        System.out.println("Product Name: " + productName);
+        System.out.println("Price: $" + price);
+    }
+
+    public static void displayTotalProducts() {
+        System.out.println("Total Products Created: " + totalProducts);
+    }
+
+    public static void main(String[] args) {
+        Product p1 = new Product("Laptop", 1200.50);
+        Product p2 = new Product("Smartphone", 799.99);
+
+        p1.displayProductDetails();
+        System.out.println();
+        p2.displayProductDetails();
+        System.out.println();
+
+        Product.displayTotalProducts();
+    }
+}
