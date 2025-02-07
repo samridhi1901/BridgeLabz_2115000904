e2d617644f7a40cd323bf843eb7b75388d06a873
samridhi singh <gurjarsam6@gmail.com>
2025-02-07 22:44:34 +0530

    Library

---
+++ bl_ass11/LibraryTest.java
@@ -0,0 +1,37 @@
+class Book {
+    String title;
+    int publicationYear;
+
+    public Book(String title, int publicationYear) {
+        this.title = title;
+        this.publicationYear = publicationYear;
+    }
+
+    public void displayInfo() {
+        System.out.println("Title: " + title + ", Publication Year: " + publicationYear);
+    }
+}
+
+class Author extends Book {
+    String name;
+    String bio;
+
+    public Author(String title, int publicationYear, String name, String bio) {
+        super(title, publicationYear);
+        this.name = name;
+        this.bio = bio;
+    }
+
+    @Override
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("Author: " + name + ", Bio: " + bio);
+    }
+}
+
+public class LibraryTest {
+    public static void main(String[] args) {
+        Author book = new Author("The Great Adventure", 2020, "John Doe", "A famous novelist known for adventure stories.");
+        book.displayInfo();
+    }
+}
