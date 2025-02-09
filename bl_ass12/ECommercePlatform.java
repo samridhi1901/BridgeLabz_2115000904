90eec65f43cca234ac596e7b666ee2669e966613
samridhi singh <gurjarsam6@gmail.com>
2025-02-09 12:48:49 +0530

    ECommerce

---
+++ bl_ass12/ECommercePlatform.java
@@ -0,0 +1,100 @@
+import java.util.ArrayList;
+import java.util.List;
+
+abstract class Product {
+    private int productId;
+    private String name;
+    private double price;
+
+    public Product(int productId, String name, double price) {
+        this.productId = productId;
+        this.name = name;
+        this.price = price;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public abstract double calculateDiscount();
+}
+
+interface Taxable {
+    double calculateTax();
+    String getTaxDetails();
+}
+
+class Electronics extends Product implements Taxable {
+    public Electronics(int productId, String name, double price) {
+        super(productId, name, price);
+    }
+
+    public double calculateDiscount() {
+        return getPrice() * 0.10;
+    }
+
+    public double calculateTax() {
+        return getPrice() * 0.15;
+    }
+
+    public String getTaxDetails() {
+        return "15% VAT applied";
+    }
+}
+
+class Clothing extends Product implements Taxable {
+    public Clothing(int productId, String name, double price) {
+        super(productId, name, price);
+    }
+
+    public double calculateDiscount() {
+        return getPrice() * 0.20;
+    }
+
+    public double calculateTax() {
+        return getPrice() * 0.05;
+    }
+
+    public String getTaxDetails() {
+        return "5% sales tax applied";
+    }
+}
+
+class Groceries extends Product {
+    public Groceries(int productId, String name, double price) {
+        super(productId, name, price);
+    }
+
+    public double calculateDiscount() {
+        return getPrice() * 0.05;
+    }
+}
+
+public class ECommercePlatform {
+    public static void main(String[] args) {
+        List<Product> products = new ArrayList<>();
+        products.add(new Electronics(201, "Laptop", 1200));
+        products.add(new Clothing(202, "T-Shirt", 50));
+        products.add(new Groceries(203, "Apple", 2));
+        
+        for (Product product : products) {
+            double discount = product.calculateDiscount();
+            double tax = (product instanceof Taxable) ? ((Taxable) product).calculateTax() : 0;
+            double finalPrice = product.getPrice() + tax - discount;
+            
+            System.out.println("Product: " + product.getName() + ", Final Price: " + finalPrice);
+        }
+    }
+}
