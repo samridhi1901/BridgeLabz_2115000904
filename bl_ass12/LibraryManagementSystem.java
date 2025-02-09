5a9d7ceae3a392e0bec5e415802372ca9b5740ee
samridhi singh <gurjarsam6@gmail.com>
2025-02-09 13:03:57 +0530

    Libraryyy

---
+++ bl_ass12/LibraryManagementSystem.java
@@ -0,0 +1,97 @@
+import java.util.ArrayList;
+import java.util.List;
+
+abstract class LibraryItem {
+    private String itemId;
+    private String title;
+    private String author;
+
+    public LibraryItem(String itemId, String title, String author) {
+        this.itemId = itemId;
+        this.title = title;
+        this.author = author;
+    }
+
+    public String getItemDetails() {
+        return "ID: " + itemId + ", Title: " + title + ", Author: " + author;
+    }
+
+    public abstract int getLoanDuration();
+}
+
+interface Reservable {
+    void reserveItem();
+    boolean checkAvailability();
+}
+
+class Book extends LibraryItem implements Reservable {
+    public Book(String itemId, String title, String author) {
+        super(itemId, title, author);
+    }
+
+    public int getLoanDuration() {
+        return 14;
+    }
+
+    public void reserveItem() {
+        System.out.println("Book reserved: " + getItemDetails());
+    }
+
+    public boolean checkAvailability() {
+        return true;
+    }
+}
+
+class Magazine extends LibraryItem implements Reservable {
+    public Magazine(String itemId, String title, String author) {
+        super(itemId, title, author);
+    }
+
+    public int getLoanDuration() {
+        return 7;
+    }
+
+    public void reserveItem() {
+        System.out.println("Magazine reserved: " + getItemDetails());
+    }
+
+    public boolean checkAvailability() {
+        return true;
+    }
+}
+
+class DVD extends LibraryItem implements Reservable {
+    public DVD(String itemId, String title, String author) {
+        super(itemId, title, author);
+    }
+
+    public int getLoanDuration() {
+        return 5;
+    }
+
+    public void reserveItem() {
+        System.out.println("DVD reserved: " + getItemDetails());
+    }
+
+    public boolean checkAvailability() {
+        return true;
+    }
+}
+
+public class LibraryManagementSystem {
+    public static void main(String[] args) {
+        List<LibraryItem> items = new ArrayList<>();
+        items.add(new Book("B101", "Java Basics", "Sam"));
+        items.add(new Magazine("M202", "Tech Monthly", "Anu"));
+        items.add(new DVD("D303", "AI Revolution", "Sam"));
+        
+        for (LibraryItem item : items) {
+            System.out.println(item.getItemDetails());
+            System.out.println("Loan Duration: " + item.getLoanDuration() + " days");
+            if (item instanceof Reservable) {
+                ((Reservable) item).reserveItem();
+            }
+            System.out.println();
+        }
+    }
+}
