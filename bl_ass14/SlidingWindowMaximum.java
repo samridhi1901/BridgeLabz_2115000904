7053d344acdd30e33a4eeba2c553f26983813ce7
samridhi singh <gurjarsam6@gmail.com>
2025-02-13 12:11:02 +0530

    sliding window

---
+++ bl_ass14/SlidingWindowMaximum.java
@@ -0,0 +1,54 @@
+import java.util.*;
+
+class SlidingWindowMaximum {
+    static int[] maxSlidingWindow(int[] nums, int k) {
+        if (nums.length == 0) return new int[0];
+
+        int n = nums.length;
+        int[] result = new int[n - k + 1];
+        Deque<Integer> deque = new LinkedList<>();
+
+        for (int i = 0; i < n; i++) {
+            if (!deque.isEmpty() && deque.peekFirst() < i - k + 1) {
+                deque.pollFirst();
+            }
+
+            while (!deque.isEmpty() && nums[deque.peekLast()] <= nums[i]) {
+                deque.pollLast();
+            }
+
+            deque.offerLast(i);
+
+            if (i >= k - 1) {
+                result[i - k + 1] = nums[deque.peekFirst()];
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of elements in the array: ");
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+
+        System.out.println("Enter the elements:");
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter window size k: ");
+        int k = sc.nextInt();
+
+        int[] result = maxSlidingWindow(nums, k);
+
+        System.out.println("Maximum in each sliding window:");
+        for (int max : result) {
+            System.out.print(max + " ");
+        }
+
+        sc.close();
+    }
+}
