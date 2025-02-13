afc63be4b03deac11e775619b53932b493134d9a
samridhi singh <gurjarsam6@gmail.com>
2025-02-13 16:38:29 +0530

    zero sum subarray

---
+++ bl_ass14/ZeroSumSubarrays.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class ZeroSumSubarrays {
+    public static void findZeroSumSubarrays(int[] arr) {
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        int sum = 0;
+        map.put(0, new ArrayList<>());
+        map.get(0).add(-1);
+
+        System.out.println("Zero sum subarrays:");
+        
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+
+            if (map.containsKey(sum)) {
+                for (int start : map.get(sum)) {
+                    System.out.println("Subarray: [" + (start + 1) + " to " + i + "]");
+                }
+            }
+
+            map.putIfAbsent(sum, new ArrayList<>());
+            map.get(sum).add(i);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter the number of elements: ");
+        int n = scanner.nextInt();
+        
+        int[] arr = new int[n];
+        System.out.println("Enter the elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        
+        findZeroSumSubarrays(arr);
+        
+        scanner.close();
+    }
+}
