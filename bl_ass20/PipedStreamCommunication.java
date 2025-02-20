9c8d9515effff8d11e182ae7d0fbd3edc8ec472c
samridhi singh <gurjarsam6@gmail.com>
2025-02-20 17:30:32 +0530

    piped stream

---
+++ bl_ass20/PipedStreamCommunication.java
@@ -0,0 +1,56 @@
+import java.io.*;
+
+class WriterThread extends Thread {
+    private PipedOutputStream pos;
+
+    public WriterThread(PipedOutputStream pos) {
+        this.pos = pos;
+    }
+
+    public void run() {
+        try {
+            String message = "Hello from WriterThread!";
+            pos.write(message.getBytes());
+            pos.close();
+        } catch (IOException e) {
+            System.out.println("Error in WriterThread: " + e.getMessage());
+        }
+    }
+}
+
+class ReaderThread extends Thread {
+    private PipedInputStream pis;
+
+    public ReaderThread(PipedInputStream pis) {
+        this.pis = pis;
+    }
+
+    public void run() {
+        try {
+            int data;
+            while ((data = pis.read()) != -1) {
+                System.out.print((char) data);
+            }
+            pis.close();
+        } catch (IOException e) {
+            System.out.println("Error in ReaderThread: " + e.getMessage());
+        }
+    }
+}
+
+public class PipedStreamCommunication {
+    public static void main(String[] args) {
+        try {
+            PipedOutputStream pos = new PipedOutputStream();
+            PipedInputStream pis = new PipedInputStream(pos);
+
+            Thread writer = new WriterThread(pos);
+            Thread reader = new ReaderThread(pis);
+
+            writer.start();
+            reader.start();
+        } catch (IOException e) {
+            System.out.println("Error in setting up piped streams: " + e.getMessage());
+        }
+    }
+}
