4e055c63fd4bdb3e5e17a6e7b06c4bc32430f76f
samridhi singh <gurjarsam6@gmail.com>
2025-02-20 16:43:31 +0530

    Invert Map

---
+++ bl_ass19/InvertMap.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class InvertMap {
+    public static <K, V> Map<V, List<K>> invertMap(Map<K, V> originalMap) {
+        Map<V, List<K>> invertedMap = new HashMap<>();
+
+        for (Map.Entry<K, V> entry : originalMap.entrySet()) {
+            V value = entry.getValue();
+            K key = entry.getKey();
+            invertedMap.computeIfAbsent(value, k -> new ArrayList<>()).add(key);
+        }
+
+        return invertedMap;
+    }
+
+    public static void main(String[] args) {
+        Map<String, Integer> inputMap = new HashMap<>();
+        inputMap.put("A", 1);
+        inputMap.put("B", 2);
+        inputMap.put("C", 1);
+
+        Map<Integer, List<String>> inverted = invertMap(inputMap);
+        System.out.println(inverted);
+    }
+}
