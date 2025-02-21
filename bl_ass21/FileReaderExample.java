d8ef8c5341b7d78bc5d24c1e86f8d17014b14a60
samridhi singh <gurjarsam6@gmail.com>
2025-02-21 15:03:11 +0530

    file reader example

---
+++ bl_ass21/FileReaderExample.java
@@ -0,0 +1,18 @@
+import java.io.*;
+
+public class FileReaderExample {
+    public static void main(String[] args) {
+        String fileName = "data.txt";
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        } catch (IOException e) {
+            System.out.println("An error occurred while reading the file");
+        }
+    }
+}
