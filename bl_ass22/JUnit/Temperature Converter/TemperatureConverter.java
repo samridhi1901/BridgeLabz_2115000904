ee53db0cdd12ea072674e08180218643ebe304a5
samridhi singh <gurjarsam6@gmail.com>
2025-02-23 01:40:31 +0530

    Temperature

---
+++ bl_ass22/JUnit/Temperature Converter/TemperatureConverter.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class TemperatureConverter {
+
+    public static double celsiusToFahrenheit(double celsius) {
+        return (celsius * 9 / 5) + 32;
+    }
+
+    public static double fahrenheitToCelsius(double fahrenheit) {
+        return (fahrenheit - 32) * 5 / 9;
+    }
+}
